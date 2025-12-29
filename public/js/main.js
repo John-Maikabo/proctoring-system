d9b8aee3042660269bb618eeca20aaf8581bc31f
John-Maikabo <maikaboj@gmail.com>
2025-12-29 12:03:05 +0200

    TURN servers for cross-network connectivity

--- public/js/main.js
+++ public/js/main.js
@@ -728,13 +728,14 @@ class ProctoringApp {
 const config = {
     iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:global.stun.twilio.com:3478' },
-        // OpenRelay (free, public)
+        { urls: 'stun:global.stun.twilio.com:3478' }, // Free STUN only
+        // OpenRelay - Free public TURN (static credentials)
         { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
         { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
         { urls: 'turn:openrelay.metered.ca:443?transport=tcp', username: 'openrelayproject', credential: 'openrelayproject' },
     ],
-    iceTransportPolicy: 'all'
+    iceTransportPolicy: 'all',
+    iceCandidatePoolSize: 10
 };
 
         

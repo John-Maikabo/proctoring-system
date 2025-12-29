9432c731484ae883aed505dba824663415999bd4
John-Maikabo <maikaboj@gmail.com>
2025-12-29 12:25:32 +0200

    FIXED: Added TURN servers for cross-network connectivity

--- public/js/main.js
+++ public/js/main.js
@@ -725,18 +725,26 @@ class ProctoringApp {
         console.log(`🚀 Creating RTCPeerConnection for ${peerName} (${peerId})`);
         
         // FIXED: Added TURN servers for cross-network connectivity
-const config = {
-    iceServers: [
-        { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:global.stun.twilio.com:3478' }, // Free STUN only
-        // OpenRelay - Free public TURN (static credentials)
-        { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
-        { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
-        { urls: 'turn:openrelay.metered.ca:443?transport=tcp', username: 'openrelayproject', credential: 'openrelayproject' },
+const iceServers = [
+  {
+    urls: [
+      "stun:stun.l.google.com:19302",
+      "stun:global.stun.twilio.com:3478"
+    ]
+  },
+  {
+    urls: [
+      "turn:openrelay.metered.ca:80",
+      "turn:openrelay.metered.ca:443",
+      "turn:openrelay.metered.ca:443?transport=tcp"
     ],
-    iceTransportPolicy: 'all',
-    iceCandidatePoolSize: 10
-};
+    username: "openrelayproject",
+    credential: "openrelayproject"
+  }
+];
+
+const config = { iceServers }; // You can add iceTransportPolicy: 'all' here too
+const peerConnection = new RTCPeerConnection(config);
 
         
         const peerConnection = new RTCPeerConnection(config);
